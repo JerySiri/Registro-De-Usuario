92004ffaf6286f728e6e76165fd4726010c124a7
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-02-03 02:49:59 -0400

    Registro de roles. Closes #4.

---
+++ Tarea_3_RegistroDeUsuario/RegistroRolesForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Tarea_3_RegistroDeUsuario
+{
+    partial class RegistroRolesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroRolesForm));
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.DescripcionLabel = new System.Windows.Forms.Label();
+            this.FechaCreacionLabel = new System.Windows.Forms.Label();
+            this.RolIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.DescripcionTextBox = new System.Windows.Forms.TextBox();
+            this.FechaCreacionDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.RolIdNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(17, 14);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(18, 15);
+            this.IdLabel.TabIndex = 0;
+            this.IdLabel.Text = "ID";
+            // 
+            // DescripcionLabel
+            // 
+            this.DescripcionLabel.AutoSize = true;
+            this.DescripcionLabel.Location = new System.Drawing.Point(17, 46);
+            this.DescripcionLabel.Name = "DescripcionLabel";
+            this.DescripcionLabel.Size = new System.Drawing.Size(69, 15);
+            this.DescripcionLabel.TabIndex = 1;
+            this.DescripcionLabel.Text = "Descripcion";
+            // 
+            // FechaCreacionLabel
+            // 
+            this.FechaCreacionLabel.AutoSize = true;
+            this.FechaCreacionLabel.Location = new System.Drawing.Point(16, 78);
+            this.FechaCreacionLabel.Name = "FechaCreacionLabel";
+            this.FechaCreacionLabel.Size = new System.Drawing.Size(105, 15);
+            this.FechaCreacionLabel.TabIndex = 2;
+            this.FechaCreacionLabel.Text = "Fecha De Creacion";
+            // 
+            // RolIdNumericUpDown
+            // 
+            this.RolIdNumericUpDown.Location = new System.Drawing.Point(133, 12);
+            this.RolIdNumericUpDown.Name = "RolIdNumericUpDown";
+            this.RolIdNumericUpDown.Size = new System.Drawing.Size(40, 23);
+            this.RolIdNumericUpDown.TabIndex = 4;
+            // 
+            // DescripcionTextBox
+            // 
+            this.DescripcionTextBox.Location = new System.Drawing.Point(134, 43);
+            this.DescripcionTextBox.Name = "DescripcionTextBox";
+            this.DescripcionTextBox.Size = new System.Drawing.Size(179, 23);
+            this.DescripcionTextBox.TabIndex = 5;
+            // 
+            // FechaCreacionDateTimePicker
+            // 
+            this.FechaCreacionDateTimePicker.CustomFormat = "dd/mm/yyyy";
+            this.FechaCreacionDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.FechaCreacionDateTimePicker.Location = new System.Drawing.Point(134, 72);
+            this.FechaCreacionDateTimePicker.Name = "FechaCreacionDateTimePicker";
+            this.FechaCreacionDateTimePicker.Size = new System.Drawing.Size(100, 23);
+            this.FechaCreacionDateTimePicker.TabIndex = 6;
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Image = ((System.Drawing.Image)(resources.GetObject("NuevoButton.Image")));
+            this.NuevoButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.NuevoButton.Location = new System.Drawing.Point(68, 114);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(65, 40);
+            this.NuevoButton.TabIndex = 7;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            this.NuevoButton.Click += new System.EventHandler(this.NuevoButton_Click);
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Image = ((System.Drawing.Image)(resources.GetObject("GuardarButton.Image")));
+            this.GuardarButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.GuardarButton.Location = new System.Drawing.Point(139, 114);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(66, 40);
+            this.GuardarButton.TabIndex = 8;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            this.GuardarButton.Click += new System.EventHandler(this.GuardarButton_Click);
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Image = ((System.Drawing.Image)(resources.GetObject("EliminarButton.Image")));
+            this.EliminarButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.EliminarButton.Location = new System.Drawing.Point(211, 114);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(65, 40);
+            this.EliminarButton.TabIndex = 9;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            this.EliminarButton.Click += new System.EventHandler(this.EliminarButton_Click);
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButton.Image")));
+            this.BuscarButton.Location = new System.Drawing.Point(179, 12);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(26, 23);
+            this.BuscarButton.TabIndex = 10;
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            this.BuscarButton.Click += new System.EventHandler(this.BuscarButton_Click);
+            // 
+            // RegistroRolesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 164);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.FechaCreacionDateTimePicker);
+            this.Controls.Add(this.DescripcionTextBox);
+            this.Controls.Add(this.RolIdNumericUpDown);
+            this.Controls.Add(this.FechaCreacionLabel);
+            this.Controls.Add(this.DescripcionLabel);
+            this.Controls.Add(this.IdLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RegistroRolesForm";
+            this.Text = "Registro De Roles";
+            this.Load += new System.EventHandler(this.RegistroRolesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RolIdNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.Label DescripcionLabel;
+        private System.Windows.Forms.Label FechaCreacionLabel;
+        private System.Windows.Forms.NumericUpDown RolIdNumericUpDown;
+        private System.Windows.Forms.TextBox DescripcionTextBox;
+        private System.Windows.Forms.DateTimePicker FechaCreacionDateTimePicker;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Button BuscarButton;
+    }
+}
---
+++ Tarea_3_RegistroDeUsuario/RegistroRolesForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tarea_3_RegistroDeUsuario.BLL;
+using Tarea_3_RegistroDeUsuario.DAL;
+using Tarea_3_RegistroDeUsuario.Entidades;
+
+namespace Tarea_3_RegistroDeUsuario
+{
+    public partial class RegistroRolesForm : Form
+    {
+        public RegistroRolesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+            RolIdNumericUpDown.Value = 0;
+            DescripcionTextBox.Text = string.Empty;
+            FechaCreacionDateTimePicker.Value = DateTime.Now;
+        }
+
+        private bool LlenarCampos(int id)
+        {
+
+            Usuarios user = UsuariosBLL.Buscar(id);
+
+            if (user != null)
+            {
+                RolIdNumericUpDown.Value = user.RolId;
+                DescripcionTextBox.Text = user.Alias;
+                FechaCreacionDateTimePicker.Value = user.FechaIngreso;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private Roles LlenarClase()
+        {
+            Roles rol = new Roles();
+            try
+            {
+                rol.rolesId = (int)RolIdNumericUpDown.Value;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            rol.descripcion = DescripcionTextBox.Text;
+            rol.fechaCreacion = FechaCreacionDateTimePicker.Value;
+
+            return rol;
+        }
+        private bool EstaEnLaBD()
+        {
+            Roles rol = RolesBLL.Buscar(Convert.ToInt32((int)RolIdNumericUpDown.Value));
+            return (rol != null);
+        }
+
+        private void RegistroRolesForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, EventArgs e)
+        {
+            Roles rol;
+            bool paso = false;
+
+            if (!Validar())
+            {
+                MessageBox.Show("Campo Descripcion vacio", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                rol = LlenarClase();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dato invalido en ID", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            if (!RolesBLL.ExisteDescripcion(DescripcionTextBox.Text))
+                paso = RolesBLL.Guardar(rol);
+
+            else
+            {
+
+                MessageBox.Show("No se puede guardar el rol ya que este existe. Verifique la Descripcion", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado!!", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("No fue posible guardar!!", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+
+            if (string.IsNullOrWhiteSpace(DescripcionTextBox.Text))
+            {
+
+                DescripcionTextBox.Focus();
+                paso = false;
+            }
+            return paso;
+        }
+
+        private void EliminarButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+
+            if (RolesBLL.Eliminar((int)RolIdNumericUpDown.Value))
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("No pudo ser eliminado", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+        }
+
+        private void BuscarButton_Click(object sender, EventArgs e)
+        {
+            if (LlenarCampos(Convert.ToInt32(RolIdNumericUpDown.Value)))
+            {
+                MessageBox.Show("Encontrado!!", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Usuario No existe.", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
