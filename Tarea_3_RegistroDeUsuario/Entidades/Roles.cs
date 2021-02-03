00bd430dfd4feed3594abf16e9cdf390d2dcc75e
Jery Javier Sirei Mena <jery_sirei@ucne.edu.do>
2021-02-02 23:02:51 -0400

    Creacion de la entidad Roles y RoelesBLL. Closes #5.

---
+++ Tarea_3_RegistroDeUsuario/Entidades/Roles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_RegistroDeUsuario.Entidades
+{
+    class Roles
+    {
+        [Key]
+        public int rolesId { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fechaCreacion { get; set; }
+
+
+        public Roles()
+        {
+            rolesId = 0;
+            fechaCreacion = DateTime.Now;
+            descripcion = string.Empty;
+
+        }
+
+    }
+}
